729828c4873dcf9099efd7812e399b63ea219b4b
Thibaut Barrère <thibaut.barrere@gmail.com>
2019-05-17 11:01:14 +0200

    Create bulk_upsert.rb

---
+++ bulk_upsert.rb
@@ -0,0 +1,15 @@
+require 'kiba'
+
+module Kiba
+  module Blueprints
+    module BulkUpsert
+      module_function
+
+      def setup()
+        Kiba.parse do
+
+        end
+      end
+    end
+  end
+end
